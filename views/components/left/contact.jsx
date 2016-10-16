9f51d62fcac88bcdbae5e44403c0079f254eb0a4
Âgte <agte@iMac-de-Agte.local>
2016-10-16 13:45:55 +0200

    Add target blank to links

--- views/components/left/contact.jsx
+++ views/components/left/contact.jsx
@@ -4,13 +4,13 @@ class Contact extends React.Component{
 
     render() {
         return  <div className="contact" title="See ahrgomez's github account">
-                    <a className="git" href="htts://github.com/ahrgomez">
+                    <a className="git" target="_blank" href="htts://github.com/ahrgomez">
                         <i className="fa fa-github" />
                     </a>
-                    <a className="linkedin" href="https://es.linkedin.com/in/alejandro-hernández-a056233b" title="See ahrgomez's linkedin CV">
+                    <a className="linkedin"  target="_blank" href="https://es.linkedin.com/in/alejandro-hernández-a056233b" title="See ahrgomez's linkedin CV">
                         <i className="fa fa-linkedin" />
                     </a>
-                    <a className="mail" href="mailto:contact@ahrgomez.com" title="Send an email to ahrgomez">
+                    <a className="mail" target="_blank" href="mailto:contact@ahrgomez.com" title="Send an email to ahrgomez">
                         <i className="fa fa-envelope" />
                     </a>
                 </div>
